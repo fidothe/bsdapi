c5f1fc5d06243fe76240db37ce552014e6249f9f
Scott Frazer <sfrazer@bluestatedigital.com>
2010-05-04 16:52:30 -0400

    added all remaining API calls minus the VAN stuff.

--- PyAPI.py
+++ PyAPI.py
@@ -54,7 +54,7 @@ class PyAPI:
         query = OrderedDict([('ext_type', str(ext_type))])
         query.update(cons_id__ext_id);
         url_secure = self._generateRequest('/cons/set_ext_ids')
-        return self._makePOSTRequest(url_secure, urllib.parse.urlencode(query))
+        return self._makePOSTRequest(url_secure, query)
 
     def cons_deleteConstituentsById(self, cons_ids):
         query = {'cons_ids': ','.join([str(cons) for cons in cons_ids])}
@@ -71,7 +71,77 @@ class PyAPI:
             query['filter'] =  str(PyAPIFilters(filters))
 
         url_secure = self._generateRequest('/cons/get_bulk_constituent_data')
-        return self._makePOSTRequest(url_secure, urllib.parse.urlencode(query))
+        return self._makePOSTRequest(url_secure, query)
+
+    def cons_setConstituentData(self, xml_data):
+        url_secure = self._generateRequest('/cons/set_constituent_data')
+        return self._makePOSTRequest(url_secure, xml_data)
+
+    def cons_group_listConstituentGroups(self):
+        url_secure = self._generateRequest('/cons_group/list_constituent_groups')
+        return self._makeGETRequest(url_secure)
+
+    def cons_group_getConstituentGroup(self, cons_group_id):
+        query = {'cons_group_id': str(cons_group_id)}
+        url_secure = self._generateRequest('/cons_group/get_constituent_group', query)
+        return self._makeGETRequest(url_secure)
+
+    def cons_group_addConstituentGroup(self, xml_data):
+        url_secure = self._generateRequest('/cons/add_constituent_group')
+        return self._makePOSTRequest(url_secure, xml_data)
+
+    def cons_group_deleteConstituentGroup(self, cons_group_ids):
+        query = {'cons_group_ids': ','.join([str(c) for c in cons_group_ids])}
+        url_secure = self._generateRequest('/cons_group/delete_constituent_group', query)
+        return self._makeGETRequest(url_secure)
+
+    def cons_group_getConsIdsForGroup(self, cons_group_id):
+        query = {'cons_group_ids': str(cons_group_id)}
+        url_secure = self._generateRequest('/cons_group/get_cons_ids_for_group', query)
+        return self._makeGETRequest(url_secure)
+
+    def cons_group_getExtIdsForGroup(self, cons_group_id, ext_type):
+        query = {'cons_group_ids': str(cons_group_id), 'ext_type': ext_type}
+        url_secure = self._generateRequest('/cons_group/get_ext_ids_for_group', query)
+        return self._makeGETRequest(url_secure)
+
+    def cons_group_setExtIdsForGroup(self, cons_group_id, ext_type, ext_ids):
+        query = {'cons_group_ids': str(cons_group_id),
+                 'ext_type': ext_type),
+                 'ext_ids': ','.join([str(ext) for ext in ext_ids])}
+
+        url_secure = self._generateRequest('/cons_group/set_ext_ids_for_group')
+        return self._makePOSTRequest(url_secure, query)
+
+    def cons_group_addConsIdsToGroup(self, cons_group_id, cons_ids):
+        query = {'cons_group_ids': str(cons_group_id),
+                 'cons_ids': ','.join([str(cons) for cons in cons_ids])}
+
+        url_secure = self._generateRequest('/cons_group/add_cons_ids_to_group')
+        return self._makePOSTRequest(url_secure, query)
+
+    def cons_group_addExtIdsToGroup(self, cons_group_id, ext_type, ext_ids):
+        query = {'cons_group_ids': str(cons_group_id),
+                 'ext_type': ext_type),
+                 'ext_ids': ','.join([str(ext) for ext in ext_ids])}
+
+        url_secure = self._generateRequest('/cons_group/add_ext_ids_to_group')
+        return self._makePOSTRequest(url_secure, query)
+
+    def cons_group_removeConsIdsToGroup(self, cons_group_id, cons_ids):
+        query = {'cons_group_ids': str(cons_group_id),
+                 'cons_ids': ','.join([str(cons) for cons in cons_ids])}
+
+        url_secure = self._generateRequest('/cons_group/remove_cons_ids_to_group')
+        return self._makePOSTRequest(url_secure, query)
+
+    def cons_group_removeExtIdsToGroup(self, cons_group_id, ext_type, ext_ids):
+        query = {'cons_group_ids': str(cons_group_id),
+                 'ext_type': ext_type),
+                 'ext_ids': ','.join([str(ext) for ext in ext_ids])}
+
+        url_secure = self._generateRequest('/cons_group/remove_ext_ids_to_group')
+        return self._makePOSTRequest(url_secure, query)
 
     def circle_listCircles(self, circle_type=None, state_cd=None):
         query = {}
@@ -83,6 +153,103 @@ class PyAPI:
             query['state_cd'] = str(state_cd)
 
         url_secure = self._generateRequest('/circle/list_circles', query)
+        return self._makeGETRequest(url_secure)
+
+    def circle_getConsIdsForCircle(self, circle_id):
+        query = {'circle_id': str(circle_id)}
+        url_secure = self._generateRequest('/circle/get_cons_ids_for_circle', query)
+        return self._makeGETRequest(url_secure)
+
+    def circle_getExtIdsForCircle(self, circle_id, ext_type):
+        query = {'circle_id': str(circle_id), 'ext_type': ext_type}
+        url_secure = self._generateRequest('/circle/get_ext_ids_for_circle', query)
+        return self._makeGETRequest(url_secure)
+
+    def circle_setConsIdsForCircle(self, circle_id, cons_ids):
+        query = {'circle_id': str(circle_id),
+                 'cons_ids': ','.join([str(cons) for cons in cons_ids])}
+
+        url_secure = self._generateRequest('/circle/set_cons_ids_for_circle')
+        return self._makeGETRequest(url_secure, query)
+
+    def circle_setExtIdsForCircle(self, circle_id, ext_type, ext_ids):
+        query = {'circle_id': str(circle_id),
+                 'ext_type': ext_type,
+                 'ext_ids': ','.join([str(ext_id) for ext_id in ext_ids])}
+
+        url_secure = self._generateRequest('/circle/set_ext_ids_for_circle')
+        return self._makeGETRequest(url_secure, query)
+
+    def circle_addConsIdsForCircle(self, circle_id, cons_ids):
+        query = {'circle_id': str(circle_id),
+                 'cons_ids': ','.join([str(cons) for cons in cons_ids])}
+
+        url_secure = self._generateRequest('/circle/add_cons_ids_for_circle')
+        return self._makeGETRequest(url_secure, query)
+
+    def circle_addExtIdsForCircle(self, circle_id, ext_type, ext_ids):
+        query = {'circle_id': str(circle_id),
+                 'ext_type': ext_type,
+                 'ext_ids': ','.join([str(ext_id) for ext_id in ext_ids])}
+
+        url_secure = self._generateRequest('/circle/add_ext_ids_for_circle')
+        return self._makeGETRequest(url_secure, query)
+
+    def circle_removeConsIdsForCircle(self, circle_id, cons_ids):
+        query = {'circle_id': str(circle_id),
+                 'cons_ids': ','.join([str(cons) for cons in cons_ids])}
+
+        url_secure = self._generateRequest('/circle/remove_cons_ids_for_circle')
+        return self._makePOSTRequest(url_secure, query)
+
+    def circle_removeExtIdsForCircle(self, circle_id, ext_type, ext_ids):
+        query = {'circle_id': str(circle_id),
+                 'ext_type': ext_type,
+                 'ext_ids': ','.join([str(ext_id) for ext_id in ext_ids])}
+
+        url_secure = self._generateRequest('/circle/remove_ext_ids_for_circle')
+        return self._makePOSTRequest(url_secure, query)
+
+    def circle_moveConsIdsForCircle(self, from_circle_id, to_circle_id, cons_ids):
+        query = {'from_circle_id': str(from_circle_id),
+                 'to_circle_id': str(to_circle_id),
+                 'cons_ids': ','.join([str(cons) for cons in cons_ids])}
+
+        url_secure = self._generateRequest('/circle/move_cons_ids_for_circle')
+        return self._makePOSTRequest(url_secure, query)
+
+    def circle_moveExtIdsForCircle(self, from_circle_id, to_circle_id, ext_type, ext_ids):
+        query = {'from_circle_id': str(from_circle_id),
+                 'to_circle_id': str(to_circle_id),
+                 'ext_type': ext_type,
+                 'ext_ids': ','.join([str(ext_id) for ext_id in ext_ids])}
+
+        url_secure = self._generateRequest('/circle/move_ext_ids_for_circle')
+        return self._makePOSTRequest(url_secure, query)
+
+    def circle_setCircleAdministrator(self, circle_id, cons_id):
+        query = {'circle_id': str(from_circle_id),
+                 'cons_id': str(to_circle_id)}
+
+        url_secure = self._generateRequest('/circle/set_circle_administrator')
+        return self._makePOSTRequest(url_secure, query)
+
+    def circle_demoteCircleAdministrator(self, circle_id, cons_id):
+        query = {'circle_id': str(from_circle_id),
+                 'cons_id': str(to_circle_id)}
+
+        url_secure = self._generateRequest('/circle/demote_circle_administrator')
+        return self._makePOSTRequest(url_secure, query)
+
+    def circle_setCircleOwner(self, circle_id, cons_id):
+        query = {'circle_id': str(from_circle_id),
+                 'cons_id': str(to_circle_id)}
+
+        url_secure = self._generateRequest('/circle/set_circle_owner')
+        return self._makePOSTRequest(url_secure, query)
+
+    def wrappers_listWrappers(self):
+        url_secure = self._generateRequest('/wrappers/list_wrappers')
         return self._makeGETRequest(url_secure)
 
     def getDeferredResults(self, deferred_id):
@@ -116,7 +283,13 @@ class PyAPI:
         connection = http.client.HTTPConnection(self.host, self.port)
         headers = {"Content-type": "application/x-www-form-urlencoded",
                    "Accept": "text/xml"}
-        connection.request('POST', url_secure.getPathAndQuery(), body, headers)
+
+        if type(body).__name__ == 'dict':
+            http_body = urllib.parse.urlencode(body)
+        else:
+            http_body = body
+
+        connection.request('POST', url_secure.getPathAndQuery(), http_body, headers)
 
         response = connection.getresponse()
         headers = response.getheaders()
