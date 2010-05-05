a53f6ca1864fb26a5e79d38427bf21a27cfee519
Scott Frazer <sfrazer@bluestatedigital.com>
2010-05-05 11:45:33 -0400

    Added readline support and BASH-like history file support.

--- Console.py
+++ Console.py
@@ -3,18 +3,19 @@
 from os.path import exists
 from optparse import OptionParser
 import http.client
-import sys, code, configparser
+import sys, code, configparser, readline, atexit, os
 from xml.dom import minidom
 from URL import URL
 from RequestGenerator import RequestGenerator
 from PyAPI import PyAPI
 
-class Console:
-    def __init__(self):
-        usage = "Usage: %prog [options]"
-        version = "BSD Interactive API 1.0"
+class Console(code.InteractiveConsole):
+    def __init__(self, locals=None, filename="<console>",
+                       histfile=os.path.expanduser("~/.pyapi_history")):
+        self.usage = "Usage: %prog [options]"
+        self.version = "BSD Interactive API 1.0"
 
-        self.parser = OptionParser(usage=usage, version=version)
+        self.parser = OptionParser(usage=self.usage, version=self.version)
 
         self.parser.add_option("-v", "--verbose",
                                dest="verbose",
@@ -34,8 +35,7 @@ class Console:
                                default='/etc/bsdapi')
 
         (self.options, self.args) = self.parser.parse_args()
-    
-    def run(self):
+
         if not exists(self.options.config_file):
             sys.stderr.write("Error: Config file %s does not exist\n" % (options.config_file) )
             sys.exit(1)
@@ -43,18 +43,31 @@ class Console:
         config = configparser.RawConfigParser()
         config.read(self.options.config_file)
 
-        settings = {'basic' : {'host': 'localhost', 'port': '80'}}
+        self.settings = {'basic' : {'host': 'localhost', 'port': '80'}}
 
         for key, value in config.items('basic'):
-            settings['basic'][key] = value
+            self.settings['basic'][key] = value
 
-        api = PyAPI(settings['basic']['api_id'], settings['basic']['secret'], settings['basic']['host'], settings['basic']['port'], self.options)
+        self.api = PyAPI(self.settings['basic']['api_id'], self.settings['basic']['secret'], self.settings['basic']['host'], self.settings['basic']['port'], self.options)
 
-        local_vars = locals()
-        local_vars.update(self.__dict__)
+        code.InteractiveConsole.__init__(self, self.__dict__)
+        self.init_history(histfile)
 
+    def init_history(self, histfile):
+        readline.parse_and_bind("tab: complete")
+        if hasattr(readline, "read_history_file"):
+            try:
+                readline.read_history_file(histfile)
+            except IOError:
+                pass
+            atexit.register(self.save_history, histfile)
+
+    def save_history(self, histfile):
+        readline.write_history_file(histfile)
+
+    def run(self):
         sys.ps1 = 'api> '
-        code.interact('BSD Interactive API', local=local_vars)
+        self.interact('BSD Interactive API')
 
 if __name__ == '__main__':
     console = Console()
