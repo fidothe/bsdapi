43c85dc02033cb5d3887f13e1f16fca7f0e82398
Hanna Mendes Levitin <hlevitin@bluestatedigital.com>
2012-01-07 14:54:04 -0500

    pretty print json

--- python/bsdapi/ApiResult.py
+++ python/bsdapi/ApiResult.py
@@ -1,3 +1,5 @@
+import json
+
 class ApiResultPrettyPrintable:
     def __init__(self, styler):
         self.__dict__.update(locals())
@@ -13,11 +15,21 @@ class ApiResultPrettyPrintable:
         status_str = "%s %s %s" % (apiResult.http_version, str(apiResult.http_status), apiResult.http_reason)
         headers_str = '\n'.join(['%s: %s' % (k, v) for k, v in apiResult.headers]) + '\n'
 
-        full_str = "%s\n%s\n%s" % (self.styler.color(status_str, color), self.styler.color(headers_str, 'purple'), apiResult.body)
+        ''' assume json response body and try to prettyprint, just print plain
+        response if fail'''
+        try:
+            body_str = json.dumps(json.loads(apiResult.body), sort_keys=True, indent=4) 
+        except:
+            body_str = apiResult.body
+
+        full_str = "%s\n%s\n%s" % (self.styler.color(status_str, color),
+                self.styler.color(headers_str, 'purple'),
+                body_str)
         return full_str.strip()
 
 class ApiResult:
     def __init__(self, request_url, http_response, headers, body, stringizer = None):
+        self.s=http_response
         self.http_status  = http_response.status
         self.http_reason  = http_response.reason
         self.http_version = ('HTTP/1.0' if http_response.version == 10 else 'HTTP/1.1')
